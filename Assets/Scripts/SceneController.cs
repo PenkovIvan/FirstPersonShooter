3a8bb6864ccb9ae103557e089f238740eacd1384
Ivan <dickdickenssuper@gmail.com>
2022-11-19 21:18:28 +0300

    Для ускорения врага, после действия ползунка в слайдере доплнил SceneController. Подписчик добавляется в методе Awake() и удаляется в методе OnDestroy()

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,8 +7,26 @@ public class SceneController : MonoBehaviour
    [SerializeField] private GameObject enemyPrefab;//серриализованна€ переменна€ дл€ св€зи с объектом-шаблоном
     private GameObject _enemy;//переменна€ дл€ отслеживани€ за экземпл€ром врага на сцене
 
+    public const float baseSpeed = 3.0f;
 
-   
+    void Awake()
+    {
+        Messenger<float>.AddListener(GameEvent.SPEED_CHANGE,OnEnemySpeedChange);
+    }
+    void OnDestroy()
+    {
+        Messenger<float>.RemoveListener(GameEvent.SPEED_CHANGE, OnEnemySpeedChange);
+    }
+
+    private void OnEnemySpeedChange(float val)
+    {
+        float speedEnemy = 3.0f;
+        speedEnemy = baseSpeed * val;
+    }
+
+    
+
+
     void Update()
     {
         if (_enemy == null)//пораждаем нового врага, только если враги отсутствуют
@@ -17,6 +35,8 @@ public class SceneController : MonoBehaviour
             _enemy.transform.position = new Vector3(0, 1, 0);
             float angle = Random.Range(0, 360);
             _enemy.transform.Rotate(0,angle,0);
+           
+
         }
     }
 }
