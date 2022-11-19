c4836866fc26be4987e0ddfde0827f1440f060f2
Ivan <dickdickenssuper@gmail.com>
2022-11-19 17:19:11 +0300

    Для оповещения пользовательского интерфейса о происходящем в сцене создадим систему широковещательной рассылки сообщений. Создаю сценарий с именем Messenger, код которого взял с https://github.com/ jhocking/from-unity-wiki/blob/main/Messenger.cs
    GameEvent задает константу для пары сообщений о событиях, что позволяет систематизировать сообщения, одновременно избавляя от необходимости вводить строку сообщения в разных местах.

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameEvent
+{
+    public const string ENEMY_HIT = "ENEMY HIT";
+    public const string SPEED_CHANGE = "SPEED CHANGE";
+}
